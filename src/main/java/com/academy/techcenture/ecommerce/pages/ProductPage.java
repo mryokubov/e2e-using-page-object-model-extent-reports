608c9b6d1ccaa995a89f8d42e0eee8a6319857eb
Tatiana Shakhmaykina <shakhmaykina@outlook.com>
2022-06-25 19:12:05 -0400

    finished 5 and 6,7,8 steps

--- src/main/java/com/academy/techcenture/ecommerce/pages/ProductPage.java
+++ src/main/java/com/academy/techcenture/ecommerce/pages/ProductPage.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import java.util.List;
 import java.util.Map;
 
+import static org.testng.Assert.assertEquals;
+
 public class ProductPage extends HomePage {
     public ProductPage(WebDriver driver) {
         super(driver);
@@ -66,6 +68,12 @@ public class ProductPage extends HomePage {
 
     @FindBy(xpath = "//h2[.='New comment']")
     private WebElement popUpCommentHeaderTxt;
+
+    @FindBy(xpath = "//p[contains( .,'Your comment')]")
+    private WebElement popUpMessage;
+
+    @FindBy (className = "submit")
+    private WebElement okBtn;
 
     @FindBy(id = "send_friend_button")
     private WebElement sendFriends;
@@ -163,35 +171,71 @@ public class ProductPage extends HomePage {
     @FindBy(xpath = "//a[contains(.,'Proceed to checkout')]")
     private WebElement proceedCheckOutBtn;
 
+    private String[] socialNetworksLinksExpected = {"tweet",
+            "share", "google+","pinterest"};
 
-    public void verfiyingTheProductPage(Map<String, String> data){
+
+    public void verifyingTheProductPage(Map<String, String> data) {
         Assert.assertTrue(breadcrumb.isDisplayed(), "Bread crumb is not displayed");
-        Assert.assertEquals(productHeader.getText().trim(), "\"" + data.get(""));
-
-
-
+        Assert.assertEquals(productHeader.getText().trim(), data.get("Name"), "Name isn't correct");
+        Assert.assertEquals(productReference.getText().trim(), data.get("Reference"), "Reference isn't correct");
+        Assert.assertEquals(productCondition.getText().trim(), data.get("Condition"));
+        Assert.assertEquals(shortDescriptionContent.getText().trim(), data.get("Description"), "Description isn't correct");
+        Assert.assertEquals(4, socialNetworksLinks.size(),"Number of Social links aren't 4");
+        for (int i = 0; i < socialNetworksLinks.size(); i++) {
+            assertEquals(socialNetworksLinks.get(i).getText().toLowerCase().trim(), socialNetworksLinksExpected[i],
+                    "Network Link did not match " + socialNetworksLinksExpected[i]);
+        }
+        Assert.assertEquals(priceAfterDiscount.getText().trim().replace("$",""), data.get("Price After Discount"),"Price after discount isn't correct");
+        Assert.assertEquals(priceBeforeDiscount.getText().trim().replace("$",""), data.get("Price Before Discount"),"Price before discount isn't correct");
+        Assert.assertEquals(reductionPrecent.getText().trim().replace("-",""), data.get("Discount"),"Discount isn't correct");
+        Assert.assertTrue(minusBtn.isEnabled(), "Plus btn is not enabled");
+        Assert.assertTrue(plusBtn.isEnabled(), "Minus btn is not enabled");
+        Assert.assertTrue(quantityInput.isEnabled(), "Quantity Input is not enabled");
+        if(quantityInput.getText().trim().equals(1)){
+            plusBtn.click();
+        } else if (!quantityInput.getText().trim().equals(2)) {
+            int quantity = Integer.parseInt(quantityInput.getText().trim());
+            for (int i=1; i <= quantity; i ++) {
+                minusBtn.click();
+        }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        Assert.assertTrue(sizeOptions.isEnabled(), "Size field is not enabled");
+        Assert.assertEquals(colors.size(),data.get("Available Color"), "The number of colors does not match");
+        Assert.assertTrue(wishListBtn.isEnabled(), "Wish button isn't enabled");
+        Assert.assertTrue(paymentLogos.isDisplayed(), "Payment logos isn't displayed");
+        Assert.assertTrue(dataSheetHeader.isDisplayed(), "Data Sheet Header isn't displayed");
+        Assert.assertEquals(2, tableCols.size(),"Columns aren't 2");
+        Assert.assertEquals(3, tableRows.size(),"Rows aren't 3");
+        Assert.assertEquals(moreInfoHeader.getText().trim(),"More info", "Info Header isn't correct");
+        Assert.assertEquals(moreInfoText.getText().trim(), data.get("Info Text"), "Info text isn't correct");
+        Assert.assertEquals(reviews.getText().trim(),"Reviews" , "Review header isn't correct");
+        Assert.assertEquals(reviewsText.isDisplayed() , "Review text isn't displayed");
+        Assert.assertTrue(addToCartBtn.isEnabled(), "The add button isn't enabled");
+        addToCartBtn.click();
+    }
+    public void verifyingReviewPopUp (Map<String, String> data) {
+        Assert.assertTrue(writeReview.isDisplayed(), "Review field is not displayed");
+        writeReview.click();
+        Assert.assertEquals(reviewHeader.getText().trim().toUpperCase(),"WRITE A REVIEW", "Header of review page isn't correct");
+        Assert.assertEquals(reviewSubheading.getText().trim().toUpperCase(),"WRITE A REVIEW", "Sub-header of review page isn't correct");
+        Assert.assertEquals(reviewProductName.getText().trim(), data.get("Name"), "Name of review page isn't correct");
+        Assert.assertEquals(reviewDescription.getText().trim(), data.get("Description"), "Description of review page isn't correct");
+        Assert.assertEquals(5, reviewStars.size(), "Number of stars isn't 5");
+        Assert.assertTrue(reviewCancelBtn.isEnabled(), "Cancel button isn't displayed ");
+        Assert.assertTrue(reviewCommentTitleInput.isEnabled(), "Comment title field of review page isn't displayed ");
+        reviewCommentTitleInput.click();
+        reviewCommentTitleInput.sendKeys("not satisfied with the order");
+        Assert.assertTrue(reviewCommentInput.isEnabled(), "Input field of review page isn't displayed ");
+        reviewCommentInput.click();
+        reviewCommentInput.sendKeys("This seller is terrible. Don't think to buy this");
+        Assert.assertTrue(submitNewMessage.isEnabled(), "Submit button of review page isn't displayed ");
+        submitNewMessage.click();
+        Assert.assertEquals(popUpCommentHeaderTxt.getText().trim(),"New comment", "Title of pop-up verifying message isn't correct ");
+        Assert.assertEquals(popUpMessage.getText().trim(),"Your comment has been added and will be available once approved by a moderator", "Message of pop-up verifying message isn't correct ");
+        Assert.assertTrue(okBtn.isEnabled(), "Ok button isn't displayed on verifying message");
+        okBtn.click();
+    }
 
 
 
